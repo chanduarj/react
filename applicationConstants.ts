dfd3d7283a94eafdfa407e38e0022858283cd69d
chanduarj <chanduarj@gmail.com>
2017-09-19 03:45:55 +0530

    Add files via upload

---
+++ applicationConstants.ts
@@ -0,0 +1,12 @@
+export default class ApplicationConstants {
+
+    static asc = "ASC";
+    static desc = "DESC";
+    static filter = "Filter";
+    static numericAsc = "Sort Asc";
+    static numericDesc = "Sort Desc";
+    static textAsc = "Sort A-Z";
+    static textDesc = "Sort Z-A";
+    static NaN = "NaN";
+
+}
---
+++ data.service.ts
@@ -0,0 +1,32 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import 'rxjs/Rx';
+
+@Injectable()
+export class DataService {
+
+  private http: Http;
+
+  private getHeaders() {
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+    headers.append('Access-Control-Allow-Origin', '*'); /*cross origin is handled in API side*/
+    return headers;
+  }
+
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  public httpPost(_data, _apiUrl) {
+    let options = new RequestOptions(this.getHeaders());
+    let body = JSON.parse(_data);
+    return this.http.post(_apiUrl, body, options).map((res: Response) => res);
+  }
+
+  public httpGet(_apiUrl) {
+    let options = new RequestOptions(this.getHeaders());
+    return this.http.get(_apiUrl, options).map((res: Response) => res);
+  }
+}
